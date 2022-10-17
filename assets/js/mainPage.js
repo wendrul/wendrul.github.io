fd368d1e3baa0271c2548ff81755ed3c4784eda6
Wendrul <dethomassin.etienne@gmail.com>
2022-10-17 21:49:09 +0200

    Add gksdud

--- assets/js/mainPage.js
+++ assets/js/mainPage.js
@@ -1,11 +1,26 @@
 import Typed from "/node_modules/typed.js/src/typed.js"
+// import {gksdud, reverseGksdud} from "/gksdud/lib/gksdud.js"
 
 let options = {
-    typeSpeed: 200,
+    typeSpeed: 40,
     stringsElement: '#intro-strings',
     loop: true,
+    shuffle: true,
+    preStringTyped: preStringTyped,
+    onStart: (c) => console.log("pussiboi"),
     smartBackspace: false,
-    backSpeed: 600
+    backSpeed: 40,
+    backDelay: 10000
   };
   
 let typed = new Typed('#intro', options);
+
+function isKorean(str) {
+  return true;
+}
+
+function preStringTyped (arrayPos, self) {
+  // if (isKorean(self.strings[arrayPos])) {
+
+  // }
+}
