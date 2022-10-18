ed934393a1a36ba99c335bde787ef47fa4d2feaf
wendrul <dethomassin.etienne@gmail.com>
2022-10-18 17:46:51 +0200

    Add gksdud library

---
+++ gksdud/lib/gksdud.d.ts
@@ -0,0 +1,6 @@
+export = gksdud;
+declare function gksdud(sentences: string): string;
+declare function reverseGksdud(sentences: string): string;
+
+module.exports.gksdud = gksdud;
+module.exports.reverseGksdud = reverseGksdud;
