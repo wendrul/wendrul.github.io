8af1a72151556670d91fd814e5880741fe74f9e2
wendrul <dethomassin.etienne@gmail.com>
2022-10-26 16:27:32 +0200

    Add social buttons, pause typing on scroll out

--- assets/js/mainPage.js
+++ assets/js/mainPage.js
@@ -1,17 +1,30 @@
 import { KoreanIMEString } from "./TypedKorean.js";
-import Typed from "/node_modules/typed.js/src/typed.js"
-// import {gksdud, reverseGksdud} from "/gksdud/lib/gksdud.js"
+import Typed from "/node_modules/typed.js/src/typed.js";
 
 let options = {
-    stringsElement: '#intro-strings',
-    typeSpeed: 60,
-    loop: true,
-    shuffle: true,
-    smartBackspace: false,
-    backSpeed: 40,
-    backDelay: 3000
-  };
-  
-let typed = new Typed('#intro', options);
-// typed.stop();
+  stringsElement: "#intro-strings",
+  typeSpeed: 60,
+  loop: true,
+  shuffle: true,
+  smartBackspace: false,
+  backSpeed: 40,
+  backDelay: 3000,
+};
 
+const observerOptions = {
+  threshold: 1
+}
+
+let typed = new Typed("#intro", options);
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+
+    if (entry.isIntersecting) {
+      typed.start();
+      return;
+    }
+    typed.stop();
+  });
+}, observerOptions);
+
+observer.observe(document.querySelector('.intro-tophalf'));
